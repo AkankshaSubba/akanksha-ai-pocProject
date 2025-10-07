225dd47cf40cb399052d860533063c9ea73ce2c0
akanksha.subba <akanksha.subba@mphasis.com>
2025-10-07 20:24:09 +0100

    Basic app py changes for testing container

--- backend/app.py
+++ backend/app.py
@@ -1,50 +1,21 @@
-from flask import Flask, render_template,request
-import os
-from openai import AzureOpenAI
+from flask import Flask
+import logging
+import sys
 
 app = Flask(__name__)
 
-endpoint = "https://venka-mdof0Fqm-northcentralus.cognitiveservices.azure.com/"
-##model_name = "gpt-4.1"
-deployment = "gpt-4o-mini"
-subscription_key = "3CXKHVaK01iK8EF0PJplfddIoVAn0fJUF28lRq4FulGH0LSQCbTjJQQJ99BGACHrzpqXJ3w3AAAAACOGj6WK"
-api_version = "2025-01-01-preview"
+# Configure logging to stdout
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s %(name)s: %(levelname)s %(message)s')
+handler.setFormatter(formatter)
+app.logger.addHandler(handler)
+app.logger.setLevel(logging.INFO)
 
-
-client = AzureOpenAI(
-        api_version=api_version,
-        azure_endpoint=endpoint,
-        api_key=subscription_key,
-    )
-
-@app.route('/', methods=['GET','POST'])
-def index():
-    chatresponse = ''
-    text1=''
-    if request.method == 'POST':
-        prompt= request.form.get("text1")
-        text = generate(prompt)
-        print(text.choices[0].message.content)
-        chatresponse = text.choices[0].message.content
-    return render_template('index.html',chatresponse=chatresponse,text1=text1)
-
-
-def generate(prompt):
-    response = client.chat.completions.create(
-        messages=[
-            {
-                "role": "user",
-                "content": prompt,
-            }
-        ],
-        max_completion_tokens=20,
-        temperature=1.0,
-        top_p=1.0,
-        frequency_penalty=0.0,
-        presence_penalty=0.0,
-        model=deployment
-    )
-    return response
+@app.route('/')
+def hello_world():
+    app.logger.info('Hello Logger!')
+    return '<html>Hello Logger!</html>'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
