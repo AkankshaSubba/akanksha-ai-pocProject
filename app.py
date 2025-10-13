2ae357b7d1a83dfb0081257cf35c683f2eb8abb1
akanksha.subba <akanksha.subba@mphasis.com>
2025-10-13 19:17:04 +0100

    Prompt code check in

--- app.py
+++ app.py
@@ -1,21 +1,49 @@
-from flask import Flask
-import logging
-import sys
+from flask import Flask, render_template,request
+import os
+from openai import AzureOpenAI
 
 app = Flask(__name__)
 
-# Configure logging to stdout
-handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s %(name)s: %(levelname)s %(message)s')
-handler.setFormatter(formatter)
-app.logger.addHandler(handler)
-app.logger.setLevel(logging.INFO)
+endpoint = "https://ngaifpoc.cognitiveservices.azure.com/"
+##model_name = "gpt-4.1"
+deployment = "gpt-4.1"
+subscription_key = "Dr1tvfmZw9h5ponz3QBIRnYtmj8YhNsgDAoEhmrSn1d7PbbzmCYMJQQJ99BGACmepeSXJ3w3AAAAACOGOb0I"
+api_version = "2024-12-01-preview"
 
-@app.route('/')
-def hello_world():
-    app.logger.info('Hello Logger!')
-    return '<html>Hello Logger!</html>'
+client = AzureOpenAI(
+        api_version=api_version,
+        azure_endpoint=endpoint,
+        api_key=subscription_key,
+    )
+
+@app.route('/', methods=['GET','POST'])
+def index():
+    chatresponse = ''
+    text1=''
+    if request.method == 'POST':
+        prompt= request.form.get("text1")
+        text = generate(prompt)
+        print(text.choices[0].message.content)
+        chatresponse = text.choices[0].message.content
+    return render_template('index.html',chatresponse=chatresponse,text1=text1)
+
+
+def generate(prompt):
+    response = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
+        max_completion_tokens=20,
+        temperature=1.0,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+        model=deployment
+    )
+    return response
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
